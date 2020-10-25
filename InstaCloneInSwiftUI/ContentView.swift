ad6cf392887f4885b167e35f095f718b8923645d
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-10-25 20:18:17 +0200

    Initial Commit

---
+++ InstaCloneInSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InstaCloneInSwiftUI
+//
+//  Created by Ramill Ibragimov on 25.10.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InstaCloneInSwiftUI/InstaCloneInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaCloneInSwiftUIApp.swift
+//  InstaCloneInSwiftUI
+//
+//  Created by Ramill Ibragimov on 25.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct InstaCloneInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
